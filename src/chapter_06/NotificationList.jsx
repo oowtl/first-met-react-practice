2fc61ed67010250e38d46282b3d7906fac363f43
Jun <oowtl7425@gmail.com>
2024-03-20 21:48:11 +0900

    feat: chapter 6 - Notification 컴포넌트 - NotificationList 컴포넌트

---
+++ src/chapter_06/NotificationList.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Notification from "./Notification.jsx";
+
+const reservedNotifications = [
+  {
+    message: "안녕하세요. 오늘 일정을 알려드립니다.",
+  },
+  {
+    message: "점심 식사 시간입니다.",
+  },
+  {
+    message: "이제 곧 미팅이 시작됩니다.",
+  },
+];
+
+var timer;
+
+export default class NotificationList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notifications: [],
+    };
+  }
+
+  componentDidMount() {
+    const { notifications } = this.state;
+
+    timer = setInterval(() => {
+      if (notifications.length < reservedNotifications.length) {
+        const index = notifications.length;
+        notifications.push(reservedNotifications[index]);
+        this.setState({ notifications: notifications });
+      } else {
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
+
+  componentWillUnmounted() {
+    if (timer) {
+      clearInterval(timer);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.notifications.map((notification) => {
+          return <Notification message={notification.message} />;
+        })}
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 // import Library from "./chapter_03/Library";
 // import Clock from "./chapter_04/Clock";
-import CommentList from "./chapter_05/CommentList";
+// import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -28,9 +29,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // }, 1000);
 
 // chapter 05
+// root.render(
+//   <React.StrictMode>
+//     <CommentList />
+//   </React.StrictMode>
+// );
+
+// chapter 06
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
